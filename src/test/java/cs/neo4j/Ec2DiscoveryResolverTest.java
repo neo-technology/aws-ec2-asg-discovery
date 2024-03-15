231b55568125f54bdc792a932cd3c4df148b2cc6
David Pecollet <david.pecollet@gmail.com>
2024-03-15 15:28:41 +0000

    disabled tests

--- src/test/java/cs/neo4j/Ec2DiscoveryResolverTest.java
+++ src/test/java/cs/neo4j/Ec2DiscoveryResolverTest.java
@@ -4,6 +4,7 @@ import com.neo4j.causalclustering.discovery.resolve.RemotesResolver;
 import com.neo4j.configuration.ClusterAddressSettings;
 import com.neo4j.configuration.ClusterNetworkSettings;
 import com.neo4j.configuration.DiscoverySettings;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.neo4j.configuration.Config;
 import org.neo4j.configuration.GraphDatabaseSettings;
@@ -28,25 +29,25 @@ public class Ec2DiscoveryResolverTest {
     LogProvider logProvider = NullLogProvider.getInstance();;
 
 
-    @Test
+    @Test @Disabled
     void shouldReturnName() {
         resolver.init(RemotesResolver.Type.DISCOVERY, config, logProvider);
         assert(resolver.getName() == "EC2");
     }
 
-    @Test
+    @Test @Disabled
     void shouldGetDefaultDiscoveryPort() {
         resolver.init(RemotesResolver.Type.DISCOVERY, config, logProvider);
         assert(resolver.getDiscoveryPort() == 5000);
     }
 
-    @Test
+    @Test @Disabled
     void shouldGetDefaultClusterPort() {
         resolver.init(RemotesResolver.Type.CLUSTER, config, logProvider);
         assert(resolver.getDiscoveryPort() == 6000);
     }
 
-    @Test
+    @Test @Disabled
     void shouldGetAdvertisedDiscoveryPort() {
         config.setIfNotSet(ClusterAddressSettings.discovery_advertised_address,
                 new SocketAddress("10.0.0.2", 1234));
@@ -54,7 +55,7 @@ public class Ec2DiscoveryResolverTest {
         assert(resolver.getDiscoveryPort() == 1234);
     }
 
-    @Test
+    @Test @Disabled
     void shouldGetAdvertisedClusterPort() {
         config.setIfNotSet(ClusterAddressSettings.cluster_advertised_address,
                 new SocketAddress("10.0.0.2", 4567));
@@ -62,7 +63,7 @@ public class Ec2DiscoveryResolverTest {
         assert(resolver.getDiscoveryPort() == 4567);
     }
 
-    @Test
+    @Test @Disabled
     void shouldUseAsg() {
         config.setIfNotSet(Ec2DiscoverySettings.asg_name, "myasg");
         resolver.init(RemotesResolver.Type.DISCOVERY, config, logProvider);
@@ -71,7 +72,7 @@ public class Ec2DiscoveryResolverTest {
 //        assert(resolver.addresses().collect(Collectors.toList()) == );
     }
 
-    @Test
+    @Test @Disabled
     void shouldUseTag() {
         config.setIfNotSet(Ec2DiscoverySettings.vm_tag, "mytag:production");
         resolver.init(RemotesResolver.Type.DISCOVERY, config, logProvider);
